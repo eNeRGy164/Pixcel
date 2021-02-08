301e298664014e78111ea66f322a04ccd102ebb9
Michaël Hompus <michael@hompus.nl>
2021-02-08 15:50:54 +0100

    Support non-square images

--- Pixcel/Program.cs
+++ Pixcel/Program.cs
@@ -1,27 +1,32 @@
-﻿using OfficeOpenXml;
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using OfficeOpenXml;
 using OfficeOpenXml.Style;
 using System.Drawing;
 using System.IO;
 
 var source = args[0];
+if (!File.Exists(source)) throw new FileNotFoundException($"Input image not found", source);
 
-var bitmap = new Bitmap(64, 64);
-using var graphics = Graphics.FromImage(bitmap);
-graphics.DrawImage(new Bitmap(source), 0, 0, 64, 64);
+var sourceImage = CvInvoke.Imread(source);
+var mat = new Mat();
+CvInvoke.ResizeForFrame(sourceImage, mat, new Size(64, 64), Inter.Lanczos4, scaleDownOnly: true);
+var image = mat.ToImage<Bgr, byte>();
 
 ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 using var package = new ExcelPackage(new FileInfo(source.Replace(Path.GetExtension(source), ".xlsx")));
 var worksheet = package.Workbook.Worksheets["Pixcel"] ?? package.Workbook.Worksheets.Add("Pixcel");
 
-for (int y = 1; y <= bitmap.Width; y++)
+for (int y = 1; y <= image.Height; y++)
 {
-    worksheet.Column(y).Width = 5;
+    worksheet.Row(y).Height = 27.5;
 
-    for (int x = 1; x <= bitmap.Height; x++)
+    for (int x = 1; x <= image.Width; x++)
     {
-        worksheet.Row(x).Height = 27.5;
-        worksheet.Cells[x, y].Style.Fill.PatternType = ExcelFillStyle.Solid;
-        worksheet.Cells[x, y].Style.Fill.BackgroundColor.SetColor(bitmap.GetPixel(y - 1, x - 1));
+        worksheet.Column(x).Width = 5;
+        worksheet.Cells[y, x].Style.Fill.PatternType = ExcelFillStyle.Solid;
+        worksheet.Cells[y, x].Style.Fill.BackgroundColor.SetColor(0, image.Data[y-1, x-1, 2], image.Data[y-1, x-1, 1], image.Data[y-1, x-1, 0]);
     }
 }
 
